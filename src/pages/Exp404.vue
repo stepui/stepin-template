9185c89774a350e3d85c612261ec6be6be173068
chenghongxing <1126263215@qq.com>
2023-05-07 15:43:21 +0800

    修复：修复浏览器刷新时，页面加载动画不结束问题；

--- src/pages/Exp404.vue
+++ src/pages/Exp404.vue
@@ -10,12 +10,23 @@
 <script lang="ts" setup>
   import ReadingLoader from '@/components/loaders/ReadingLoader.vue';
   import { configPage } from 'stepin/es/tabs-view';
-  import { useRoute } from 'vue-router';
+  import { useRoute, useRouter } from 'vue-router';
+  import { useMenuStore, storeToRefs } from '@/store';
+  import { watch } from 'vue';
 
   const props = defineProps({
     loading: Boolean,
   });
   const route = useRoute();
+
+  const { loading: _loading } = storeToRefs(useMenuStore());
+
+  const router = useRouter();
+
+  // fix: 修复浏览器刷新时，页面加载动画不结束问题；(暂不知道为啥这段代码能起作用)
+  watch(_loading, () => {
+    router.push('');
+  });
 
   if (props.loading) {
     configPage(route, { title: 'loading' });
