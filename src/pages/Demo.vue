512a27e916355c08dbae53818d2fb089a9bdc178
chenghongxing <1126263215@qq.com>
2023-05-18 11:30:22 +0800

    更新：更新 stepin 版本，ThemeProvider 组件支持自适应黑暗模式；

--- src/pages/Demo.vue
+++ src/pages/Demo.vue
@@ -1,4 +1,9 @@
 <template>
-  <div ref="demo" class="demo bg-red-50 p-8 mb-4">demo</div>
+  <ThemeProvider :color="{ middle: { 'bg-base': '#f9e9f9' } }">
+    <div ref="demo" class="demo p-8 mb-4">demo</div>
+  </ThemeProvider>
 </template>
-<script lang="ts" setup></script>
+
+<script lang="ts" setup>
+  import { ThemeProvider } from 'stepin';
+</script>
