c5076beca3d567996b8fbb32c90fee6c2420c3ad
chenghongxing <1126263215@qq.com>
2023-05-18 11:27:54 +0800

    修复：修复build编译报错问题；

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,46 +1,8 @@
-import {
-  DropdownProps,
-  PaginationProps,
-  ButtonProps,
-  ConfigProviderProps,
-  TabsProps,
-  SelectProps,
-  FormProps,
-  FormItemProps,
-  ModalProps,
-  paginationProps,
-  SwitchProps,
-  CascaderProps,
-  InputProps,
-  SpinProps,
-  StatisticProps,
-  TreeSelectProps,
-} from 'ant-design-vue';
 import { AlertApi } from 'stepin/es/alert-message';
 import { MessageApi } from 'ant-design-vue/es/message';
 
 import IconFont from '@/plugins/iconfont/IconFont.vue';
 declare module 'vue' {
-  export interface GlobalComponents {
-    ADropdown: DropdownProps;
-    APagination: PaginationProps;
-    AButton: ButtonProps;
-    AConfigProvider: ConfigProviderProps;
-    ATabs: TabsProps;
-    IconFont: typeof IconFont;
-    ASelect: SelectProps;
-    AForm: FormProps;
-    AFormItem: FormItemProps;
-    AModal: ModalProps;
-    APagination: paginationProps;
-    ASwitch: SwitchProps;
-    ACascader: CascaderProps;
-    AInput: InputProps;
-    ASpin: SpinProps;
-    AStatistic: StatisticProps;
-    ATreeSelect: TreeSelectProps;
-  }
-
   export interface ComponentCustomProperties {
     $message: MessageApi;
     $alert: AlertApi;
