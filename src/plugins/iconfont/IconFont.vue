bd748ac6fd5d82e8a69ef53baa61df232e41a4d4
chenghongxing <1126263215@qq.com>
2023-08-24 14:02:19 +0800

    修复：Iconfont 组件在ant design vue 菜单内水平对齐问题；

--- src/plugins/iconfont/IconFont.vue
+++ src/plugins/iconfont/IconFont.vue
@@ -2,8 +2,8 @@
   defineProps({ name: String });
 </script>
 <template>
-  <span role="img" style="line-height: 1">
-    <svg fill="currentColor" style="vertical-align: top" width="1em" height="1em" aria-hidden="true">
+  <span role="img" class="anticon">
+    <svg fill="currentColor" width="1em" height="1em" aria-hidden="true">
       <use :xlink:href="`#${name}`"></use>
     </svg>
   </span>
